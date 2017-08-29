7e26c8f7392c777bd2e328ce71e9b9c9011af73e
Wouter Vanmulken <woutervanmulken@msn.com>
2017-08-29 20:43:55 +0200

    added the createCube

--- week 1a/onthefly.py
+++ week 1a/onthefly.py
@@ -2,6 +2,8 @@
 import vizact
 import vizshape
 import vizinfo
+import vizproximity
+import random
 
 viz.setMultiSample(4)
 viz.fov(60)
@@ -18,7 +20,7 @@ BirdEyeView.setPosition([0,25,0])
 BirdEyeView.setEuler([0,90,0])
 
 viz.startLayer(viz.LINE_STRIP)
-viz.vertexColor(viz.YELLOW)
+viz.vertexColor(viz.RED)
 lines = viz.endLayer(parent=viz.ORTHO,scene=BirdEyeWindow)
 lines.dynamic()
 
@@ -38,3 +40,74 @@ vizact.ontimer(0,UpdatePath)
 
 
 vizact.onkeydown(' ',lines.clearVertices)
+
+#Choose random locations for shapes 
+locations = [ [9,1.5,-9.5] , [-9.3,1.5,-10] , [7.5,1.5,5.5] , [3,1.5,-9.5] , [-9.4,1.5,3] , [8.8,1.5,10.5] ]
+positions = random.sample(locations,3)
+
+pyramid = vizshape.addPyramid(base=(0.5,0.5),height=0.5,pos=positions[0],color=viz.YELLOW)
+
+torus = vizshape.addTorus(axis=vizshape.AXIS_X, pos=positions[1])
+torus.texture(viz.addTexture('images/tile_wood.jpg'))
+
+box = vizshape.addBox(pos=positions[2],splitFaces=True)
+box.color(viz.SKYBLUE)
+box.color(viz.PURPLE,'left')
+box.color(viz.PURPLE,'right')
+
+#Animate shapes 
+pyramid.addAction(vizact.sequence([vizact.sizeTo(size=[2,2,2],time=1),vizact.sizeTo(size=[1,1,1],time=1)], viz.FOREVER))
+x,y,z = box.getPosition()
+box.addAction(vizact.sequence([vizact.moveTo(pos=[x,y+0.5,z],time=2),vizact.moveTo(pos=[x,y-0.5,z],time=2)], viz.FOREVER))
+torus.addAction(vizact.spin(0,1,0,45))
+
+shapes = [pyramid,torus,box]
+
+
+# Prevent shapes from rendering to birds eye window 
+for s in shapes:
+    s.renderToAllWindowsExcept([BirdEyeWindow])
+    
+    
+#Add points for found shapes 
+viz.startLayer(viz.POINTS)
+viz.pointSize(5)
+viz.vertexColor(viz.RED)
+points = viz.endLayer()
+points.disable(viz.CULLING)
+points.renderOnlyToWindows([BirdEyeWindow])
+
+#Create action for when shape is found 
+fadeColor = vizact.fadeTo(viz.BLUE,time=2)
+fadeAlpha = vizact.fadeTo(0.4,time=3)
+fade = vizact.parallel(fadeColor,fadeAlpha)
+
+def FoundShape(e):
+    """Called when the viewpoint enters the proximity area of a shape"""
+
+    # Remove shape sensor from proximity manager
+    e.manager.removeSensor(e.sensor)
+
+    # Add fade action to shape
+    shape = e.sensor.getSourceObject()
+    shape.runAction(fade)
+
+    # Add found position to points
+    points.addVertex(shape.getPosition())
+
+    # Show message when all shapes have been found
+    if not e.manager.getSensors():
+        vizinfo.InfoPanel('Good Job! Game over.',align=viz.ALIGN_CENTER,icon=False)
+    
+# Create proximity manager
+manager = vizproximity.Manager()
+
+#Use main viewpoint as proximity target
+manager.addTarget(vizproximity.Target(viz.MainView))
+
+# Add a spherical proximity sensor for each shape
+sphere = vizproximity.Sphere(2.0)
+for s in shapes:
+    sensor = vizproximity.Sensor(shape=sphere, source=s)
+    manager.addSensor(sensor)
+    manager.onEnter(sensor,FoundShape)
---
+++ week 1a/plaza part two.py
@@ -0,0 +1,32 @@
+﻿import viz
+import vizshape
+import decimal
+
+
+viz.setMultiSample(4)
+piazza = viz.addChild('piazza.osgb')
+
+def createCube(position,size):
+	xOffset = position[0]
+	yOffset = position[1]
+	zOffset = position[2]
+
+	x1 = vizshape.addQuad([size[0],size[1]],vizshape.AXIS_X)
+	x2 = vizshape.addQuad([size[0],size[1]],vizshape.AXIS_X)
+	y1 = vizshape.addQuad([size[1],size[0]],vizshape.AXIS_Y)
+	y2 = vizshape.addQuad([size[1],size[0]],vizshape.AXIS_Y)
+	z1 = vizshape.addQuad([size[2],size[1]],vizshape.AXIS_Z)
+	z2 = vizshape.addQuad([size[2],size[1]],vizshape.AXIS_Z)
+	print(size[0])
+	print(size[0]/2)
+	x1.setPosition(xOffset + (size[0]/2),yOffset,zOffset)
+	x2.setPosition(xOffset - (size[0]/2),yOffset,zOffset)
+	y1.setPosition(xOffset, yOffset+ (size[1]/2), zOffset)
+	y2.setPosition(xOffset, yOffset- (size[1]/2), zOffset)
+	z1.setPosition(xOffset, yOffset, zOffset+ (size[2]/2))
+	z2.setPosition(xOffset, yOffset, zOffset- (size[2]/2))
+
+#todo fix 1/2 being 0 since it uses ints
+createCube([1,1,1],[2,2,2]);
+
+viz.go()
