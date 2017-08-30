4ddd8e0cb56c9549e639b2f2ae4f9849b135968b
Wouter Vanmulken <woutervanmulken@msn.com>
2017-08-30 12:22:00 +0200

    Added vertex cube

--- week 1a/plaza part two.py
+++ week 1a/plaza part two.py
@@ -1,11 +1,13 @@
 ﻿import viz
 import vizshape
 import decimal
+import math
 
-
+viz.clearcolor(0.75, 0.75, 0.75)
+#vizshape.addAxes()
 viz.setMultiSample(4)
-piazza = viz.addChild('piazza.osgb')
-viz.addChild('donut.obj')
+#piazza = viz.addChild('piazza.osgb')
+#viz.addChild('donut.obj')
 def createCube(position,size):
 	xOffset = position[0]
 	yOffset = position[1]
@@ -24,19 +26,50 @@ def createCube(position,size):
 	y2.setPosition(xOffset, yOffset- (size[1]/2), zOffset)
 	z1.setPosition(xOffset, yOffset, zOffset+ (size[2]/2))
 	z2.setPosition(xOffset, yOffset, zOffset- (size[2]/2))
+	
+	return [x1,x2,y1,y2,z1,z2]
 
 
+def createCylinder(r,pos):
+    # x = cx + r * cos(a)
+    # y  = cy + r * sin(a)
 
-def createCylinder():
-    print()
+    cx = pos[0]
+    cy = pos[1]
     
-    height = 3
-    r = 2
-    #2pi * r^2
-
-
+    for degree in range(0,360):
+        x = cx + r * math.cos(degree)
+        y = cy + r * math.sin(degree)
+        a = vizshape.addQuad([1,1],vizshape.AXIS_X)
+        a.setPosition(x,y)
+    
 #todo fix 1/2 being 0 since it uses ints
-createCube([1,1,1],[2,2,2]);
-createCylinder();
+#createCube([1,1,1],[2,2,2]);
+#createCylinder(1,[1,1]);
+#vizshape.addCylinder()
+
+
+
+print(1./2)
+
+
+
+viz.startLayer(viz.POINTS)
+
+#viz.startLayer(viz.POINTS)
+#viz.vertexColor(1,0,0)
+#viz.vertex(0,1,0)
+#viz.startLayer(viz.LINES)
+#viz.vertex(-1,0,0)
+#viz.vertex(1,0,0)
+#object = viz.endLayer()
+
+viz.MainView.setPosition(0,0,-2)
+viz.startLayer(viz.POINTS)
+viz.vertexColor(0,0,0)
+viz.vertex(0,0,0) 
+viz.endLayer()
+
+
 
 viz.go()
---
+++ week 1a/vertexcube.py
@@ -0,0 +1,54 @@
+﻿import viz
+import vizshape
+
+viz.setMultiSample(4)
+viz.clearcolor(0.75, 0.75, 0.75)
+piazza = viz.addChild('piazza.osgb')
+def createCube(size):
+	x = size[0]
+	y = size[1]
+	z = size[2]
+	
+	viz.MainView.setPosition(0,-.3,-1)
+	viz.startLayer(viz.QUADS)
+	
+	viz.vertexColor(x,0,0)
+	viz.vertex(0,0,0) 
+	viz.vertex(x,0,0)
+	viz.vertex(x,0,z) 
+	viz.vertex(0,0,z)  
+
+	viz.vertexColor(0,y,0)
+	viz.vertex(0,0,0) 
+	viz.vertex(0,y,0)
+	viz.vertex(x,y,0) 
+	viz.vertex(x,0,0)
+
+	viz.vertexColor(0,0,z)
+	viz.vertex(0,0,0) 
+	viz.vertex(0,y,0)
+	viz.vertex(0,y,z) 
+	viz.vertex(0,0,z)  
+
+	viz.vertexColor(0.5,0,0)
+	viz.vertex(x,0,0) 
+	viz.vertex(x,0,z)
+	viz.vertex(x,y,z) 
+	viz.vertex(x,y,0)  
+
+	viz.vertexColor(0,0.5,)
+	viz.vertex(0,y,0)
+	viz.vertex(0,y,z)
+	viz.vertex(x,y,z) 
+	viz.vertex(x,y,0)  
+
+	viz.vertexColor(0,0,.5)
+	viz.vertex(0,0,z)
+	viz.vertex(x,0,z)
+	viz.vertex(x,y,z) 
+	viz.vertex(0,y,z)  
+
+	viz.endLayer()
+
+createCube([1,2,3])
+viz.go()
